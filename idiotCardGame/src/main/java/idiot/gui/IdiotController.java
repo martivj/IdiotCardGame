edc2e80dbb54728d8ed9007cd75ed5acb99e4035
martivj <martinvagseterjakobsen@yahoo.no>
2023-04-04 05:55:07 +0200

    java project setup

---
+++ idiotCardGame/src/main/java/idiot/gui/IdiotController.java
@@ -0,0 +1,5 @@
+package idiot.gui;
+
+public class IdiotController {
+    
+}
---
+++ idiotCardGame/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+open module idiot {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+}
